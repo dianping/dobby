a0b350619f95aa7b94b4021c14d5ede6e3ab0b6d
yixzhang <romanuszy@gmail.com>
2013-05-18 15:10:12 +0800

    delete unnecessary files
     Please enter the commit message for your changes. Lines starting

--- src/main/java/com/dianping/dobby/book/biz/BookCmds.java
+++
@@ -1,9 +0,0 @@
-package com.dianping.dobby.book.biz;
-
-public interface BookCmds {
-
-   public static final String BORROW = "borrow";
-
-   public static final String RETURN = "return";
-
-}
--- src/main/java/com/dianping/dobby/book/biz/BorrowListener.java
+++
@@ -1,6 +0,0 @@
-package com.dianping.dobby.book.biz;
-
-public interface BorrowListener {
-	public void beforeStateChange(BorrowContext ctx) throws Exception;
-	public void afterStateChange(BorrowContext ctx) throws Exception;
-}
